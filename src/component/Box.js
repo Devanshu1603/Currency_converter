cfe051cfaf8971c70d7f5a29f02eda9c51ec83a5
Devanshu Kumar Singh <console.uk123@gmail.com>
2024-08-22 22:26:12 +0530

    final commit

--- src/component/Box.js
+++ src/component/Box.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Box = ({ 
-  fromCurrency, 
-  toCurrency, 
-  onFromCurrencyChange, 
-  onToCurrencyChange, 
-  getCountryCode, 
-  exchangeRate, 
-  loading, 
+const Box = ({
+  fromCurrency,
+  toCurrency,
+  onFromCurrencyChange,
+  onToCurrencyChange,
+  getCountryCode,
+  exchangeRate,
+  loading,
   fetchExchangeRate,
   amount,
   setAmount,
-  onSwapCurrencies 
+  onSwapCurrencies
 }) => {
 
   const handleFromChange = (e) => {
@@ -33,22 +33,22 @@ const Box = ({
   const convertedAmount = exchangeRate ? (amount * exchangeRate).toFixed(2) : '0.00';
   return (
     <div className='container'>
-        <div className='box1'>
-            <div className='text' id='text2'><h1>Currency</h1>
-            <h1><b>Exchanger</b></h1></div>
-            <div className='text'><p>It is a user-friendly app that instantly converts values between different currencies with real-time exchange rates. It supports multiple currencies, ensuring accurate conversions for travelers, shoppers, and business professionals. The intuitive design makes it easy to use, offering a seamless and efficient experience.</p></div>
-            <div><button id='btn' type="button" class="btn btn-primary">Let's Check It<i class="fa-solid fa-arrow-right" style={{ marginTop:'5px'}}></i></button></div>
-        </div>
+      <div className='box1'>
+        <div className='text' id='text2'><h1>Currency</h1>
+          <h1><b>Exchanger</b></h1></div>
+        <div className='text'><p>It is a user-friendly app that instantly converts values between different currencies with real-time exchange rates. It supports multiple currencies, ensuring accurate conversions for travelers, shoppers, and business professionals. The intuitive design makes it easy to use, offering a seamless and efficient experience.</p></div>
+        <div><button id='btn' type="button" class="btn btn-primary">Let's Check It<i class="fa-solid fa-arrow-right" style={{ marginTop: '5px' }}></i></button></div>
+      </div>
       <div className="wrapper">
-        <header id='title'>Currency Converter<i class="fas fa-shekel-sign" style={{marginTop:'6px'}}></i></header>
+        <header id='title'>Currency Converter<i class="fas fa-shekel-sign" style={{ marginTop: '6px' }}></i></header>
         <form action="#">
           <div className="amount">
             <p>Enter Amount</p>
-            <input 
-              type="number" 
-              value={amount} 
-              onChange={handleAmountChange} 
-              step="any" 
+            <input
+              type="number"
+              value={amount}
+              onChange={handleAmountChange}
+              step="any"
             />
           </div>
           <div className="drop-list">
@@ -69,7 +69,7 @@ const Box = ({
                 </select>
               </div>
             </div>
-            <div className="icon" onClick={onSwapCurrencies}> 
+            <div className="icon" onClick={onSwapCurrencies}>
               <i className="fas fa-exchange"></i>
             </div>
             <div className="to">
@@ -111,7 +111,7 @@ Box.propTypes = {
   fetchExchangeRate: PropTypes.func.isRequired,
   amount: PropTypes.string.isRequired,
   setAmount: PropTypes.func.isRequired,
-  onSwapCurrencies: PropTypes.func.isRequired  
+  onSwapCurrencies: PropTypes.func.isRequired
 };
 
 export default Box;
